ad17272f4b11973c3e100ca0fc7501f389a32602
BrianBurns5555 <102313108@student.swin.edu.au>
2020-03-16 19:33:55 +1100

    Added challenge shape and music manager

---
+++ Assets/Scripts/ChallengeShape.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeShape : MonoBehaviour
+{
+    public float speed;
+    public Sprite shape;
+    public List<Sprite> shapeQueue;
+    public bool reachedPlayer;
+    private Vector3 targetScale;
+    private Vector3 cutoffScale;
+    private float initialSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialSpeed = speed;
+        cutoffScale = transform.localScale / 10;
+        targetScale = transform.localScale / 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!reachedPlayer)
+        {
+
+            speed += 0.005f;
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
+            if (transform.localScale.x < cutoffScale.x)
+            {
+                speed = initialSpeed;
+                //Reset the scale of the challenge shape. Change shape to the next one in the sequence.
+                transform.localScale = new Vector3(1, 1);
+                shape = shapeQueue[0];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TempoOutput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoOutput : MonoBehaviour
+{
+    public float tempo;
+    private float timer;
+    private float beatIncrement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beatIncrement = 60 / tempo;
+        timer = beatIncrement;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timer <= Time.time)
+        {
+            Debug.Log("beat: " + Time.time + " : " + timer);
+            timer += beatIncrement;
+            transform.GetComponent<AudioSource>().Play();
+        }
+        
+    }
+}
