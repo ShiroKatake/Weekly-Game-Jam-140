94b049af5f0f8a04c7cbf7997b48f31b2f87681a
PorridgeB <brian.b5829@gmail.com>
2020-03-18 22:00:17 +1100

    Added second stage

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Startup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1600, 1200, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
